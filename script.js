0e8a8edc7479f96b7db70e31114a75f7b5ddd882
azach99 <azach99@vt.edu>
2020-06-23 11:12:39 -0400

    adding items

---
+++ script.js
@@ -0,0 +1,387 @@
+var question0 = {
+	question: "Name another word for book",
+	answers: ["NOVEL", "STORY", "PAPERBACK", "PAMPHLET", "TOME", "VOLUME", "PUBLICATION", "8"],
+	points: [55, 16, 10, 4, 4, 3, 3, 0]
+}
+var question1 = {
+	question: "Name something you always have to keep plugged in",
+	answers: ["TV", "PHONE", "COMPUTER", "LAMP", "HEADPHONES", "COMPUTER MOUSE", "7", "8"],
+	points: [33, 25, 24, 11, 2, 2, 0, 0]
+}
+var question2 = {
+	question: "Name a Harry Potter character",
+	answers: ["HARRY POTTER", "HERMIONE", "RON", "FRED/GEORGE", "NEWT SCAMANDER", "DUMBLEDORE", "SNAPE", "HAGRID"],
+	points: [52, 16 ,14, 5, 5, 3, 2, 2]
+}
+var question3 = {
+	question: "Name a fruit you might eat in the morning",
+	answers: ["BANANA", "GRAPEFRUIT", "STRAWBERRY", "APPLE", "ORANGE", "MELON", "PEACH", "8"],
+	points: [25, 22, 19, 15, 12, 3, 2, 0]
+}
+var question4 = {
+	question: "What is the first thing most people do in the morning?",
+	answers: ["BATHROOM", "SHOWER", "BREAKFAST", "OPEN EYES", "TURN OFF ALARM/SNOOZE", "BRUSH TEETH", "GET DRESSED", "8"],
+	points: [24, 19, 18, 15, 12, 7, 2, 0]
+}
+var question5 = {
+	question: "Name something women do in a sexy way that most men to not.",
+	answers: ["STRUT/WIGGLE HIPS", "DRESS/UNDRESS", "EAT/LICK FINGERS", "SIT/CROSS LEGS", "PLAY W/ HAIR", "WINK/FLUTTER LASHES", "TALK/WHISPER", "THE LOOK/POUTY LIPS"],
+	points: [56, 12, 5, 4, 4, 3, 3, 3]
+}
+var question6 = {
+	question: "Name an animal that women like being compared to",
+	answers: ["CAT/KITTEN", "COUGAR/PANTHER", "FOX", "BIRD/PEACOCK", "CHEETAH", "THEIR DOG", "LIONESS", "TIGRESS"],
+	points: [26, 20, 13, 8, 6, 5, 4, 4]
+}
+var question7 = {
+	question: "Name the worst thing you have done on a date",
+	answers: ["SNEAK AWAY", "PUKE/GET SICK", "LIE/PRETEND TO LIKE", "FART/ON HIS LAP", "BURP/MID_KISS", "GIVE UP THE COOKIE", "HIT ON OTHER GUY"],
+	points: [22, 19, 15, 14, 11, 8, 3, 0]
+}
+var question8 = {
+	question: "An ugly man might tell a pretty woman, 'I got an ugly face but I got a great what?'",
+	answers: ["PERSONALITY/HUMOR", "BOD/BONE RANGER", "BANK ACCOUNT/WALLET", "HEART", "5", "6", "7", "8"],
+	points: [52, 24, 9, 6, 0, 0, 0, 0]
+}
+var question9 = {
+	question: "Name a way a man might greet his lover that he should never greet his mom",
+	answers: ["HI SEXY LOVER/BABE", "WET KISS", "GRAB/FONDLE HER", "SPANK HER BOOTY", "BUCK NEKKID!", "6", "7", "8"],
+	points: [35, 34, 13, 10, 6, 0, 0, 0]
+}
+var question10 = {
+	question: "Name something about his wife that makes a man say he married up",
+	answers: ["SHE'S RICH/GREAT JOB", "HER FOXY FACE", "HER BANGIN BOD", "HER SMARTS", "HER HOUSE", "SHE'S YOUNG/19", "HER COOKING", "SHE'S CLASSY"],
+	points: [37, 29, 8, 3, 3, 3, 3, 3]
+}
+var question11 = {
+	question: "Name something an ex-wife might leave on her ex-husband's doorstep when he is not home",
+	answers: ["POOP/HERS", "HIS CLOTHES/SHOES", "BILLS", "PIC/HER W/ HIS BRO", "THEIR KIDS", "LETTER/NOTE", "A DEAD ANIMAL", "COURT ORDER"],
+	points: [31, 19, 12, 10, 8, 5, 4, 3]
+}
+var question12 = {
+	question: "Name the first thing you might see when you get to Heaven",
+	answers: ["JESUS/GOD", "PEARLY GATES", "ST.PETER/ANGEL", "FAM/LOVED ONES", "CLOUDS/SKY", "MY DOG/PET", "WHITE LIGHT", "SEXY MALE STRIPPERS"],
+	points: [33, 16, 15, 14, 5, 5, 4, 3]
+}
+var question13 = {
+	question: "Name something in your life you hope you don't have in the afterlife",
+	answers: ["ALIMENT/PINWORMS", "STRESS/WORRY", "DEBT/TAXES", "BLUBBERY BOD", "A JOB", "AN EX", "BAD HAIR/BALDNRESS", "MY KIDS"],
+	points: [41, 14, 14, 7, 3, 3, 3, 3]
+}
+var question14 = {
+	question: "A man might tell his wife, I only went to the strip club for what?",
+	answers: ["THE FOOD/WINGS", "A DRINK", "BACHELOR PARTY", "BUSINESS MEETING", "$1 BILLS/CHANGE", "RESEARCH/OBSERVE", "THE GREAT MUSIC"],
+	points: [31, 28, 18, 6, 5, 4, 3]
+}
+var question15 = {
+	question: "Name a piece of sporting equipment a wife might use to kill her husband",
+	answers: ["BAT", "GOLF CLUB", "WEIGHTS/DUMBBELL", "HOCKEY STICK", "HUNTING RIFLE/GUN", "JAVELIN", "7", "8"],
+	points: [44, 21, 16, 9, 3, 2, 0, 0]
+}
+var question16 = {
+	question: "Name something of Steve Harvey/s that you would like to touch",
+	answers: ["SILKY BALD HEAD", "EVERY LAST DOLLAR", "STACHE", "HAND", "SUIT/TIE", "BUTT/GLAZED HAMS", "CAR", "FACE/BUMPER LIPS"],
+	points: [33, 17, 10, 7, 7, 4, 3, 3]
+}
+var question17 = {
+	question: "Name something men think they do perfectly until they marry and the wife shows them what idiots they are",
+	answers: ["CLEAN/LAUNDRY", "COOK/GRILL", "BRING GLEE TO THE V", "DRIVE/DIRECTIONS", "DRESS", "SHOP", "7", "8"],
+	points: [41, 25, 14, 5, 4, 2, 0, 0]
+}
+var question18 = {
+	question: "You are not a nudist, but name something you do frequently in the nude", 
+	answers: ["SHOWER/BATHE", "SLEEP", "LAUNDRY/CLEAN", "SWIM", "DANCE/TO YMCA", "LOOK IN MIRROR", "7", "8"],
+	points: [68, 17, 4, 3, 2, 2, 0, 0]
+}
+var question19 = {
+	question: "Name a fruit a teenager might practice the art of kissing on", 
+	answers: ["APPLE", "PEACH", "ORANGE", "BANANA", "MELON/WATERMELON", "LEMON", "PLUM", "8"],
+	points: [35, 22, 20, 7, 6, 4, 3, 0]
+}
+var question20 = {
+	question: "Name a beverage some people drink exclusively with breakfast", 
+	answers: ["COFFEE", "ORANGE JUICE", "MILK", "GRAPEFRUIT JUICE", "WATER", "CHAMPAGNE", "7", "8"],
+	points: [31, 30, 16, 6, 4, 2, 0, 0]
+}
+var question21 = {
+	question: "Name a part of your body that you might say has an ache",
+	answers: ["HEAD", "STOMACH", "BACK", "TOOTH", "MUSCLE", "KNEE", "EAR", "8"],
+	points: [34, 33, 11, 7, 6, 3, 2, 0]
+}
+var question22 = {
+	question: "Name something you eat that might roll away if you dropped it on the floor",
+	answers: ["MEATBALL", "PEAS", "GRAPE", "ORANGE", "APPLE", "CANDY/GUMBALL", "EGG", "8"],
+	points: [28, 21, 16, 13, 8, 5, 2, 0]
+}
+var question23 = {
+	question: "Name a chore that people put off because they have work the next day",
+	answers: ["TAKE OUT THE TRASH", "DISHES", "VACUUM", "LAUNDRY", "CLEAN BATHROOM", "DUST", "7", "8"],
+	points: [35, 30, 18, 6, 4, 3, 0, 0]
+}
+var question24 = {
+	question: "Name something in your wallet",
+	answers: ["CREDIT CARD", "MONEY/CASH", "LICENSE/ID", "CONDOM", "COUPON", "6", "7", "8"],
+	points: [31, 29, 22, 11, 3, 0, 0, 0]
+}
+var question25 = {
+	question: "Name something you might wear while driving",
+	answers: ["SEATBELT/SAFETY BELT", "SUNGLASSES", "EYEGLASSES", "GLOVES", "SHOES", "6", "7", "8"],
+	points: [33, 27, 22, 9, 5, 0, 0, 0]
+}
+var question26 = {
+	question: "Name a place that you hate going that might be more tolerable if you smoked pot at first",
+	answers: ["DENTIST/DR./ER", "WORK", "IN-LAWS' HOUSE", "STORE/MALL", "THEATER/OPERA", "CHURCH", "BAR/NIGHTCLUB", "8"],
+	points: [27, 15, 12, 9, 6, 5, 4, 0]
+}
+var question27 = {
+	question: "You are driving naked. The cops pull you over. What do you hope they do not ask you to do",
+	answers: ["STEP OUT", "SPREAD 'EM", "DUI/WALK LINE", "SHOW LICENSE", "ANSWER QUESTIONS", "'STICK 'EM UP!'", "7", "8"],
+	points: [81, 6, 4, 3, 3, 2, 0, 0]
+}
+var question28 = {
+	question: "Besides the bed, name a place people like to make love",
+	answers: ["IN THE CAR", "ON THE COUCH", "ON FLOOR/FIREPLACE", "IN SHOWER/TUB", "ON KITCHEN TABLE", "IN PARK/WOODS", "7", "8"],
+	points: [33, 18, 18, 8, 8, 6, 0, 0]
+}
+var question29 = {
+	question: "If your wife left you on Saturday Night, what would you do on Sunday Morning?",
+	answers: ["SLEEP IN", "WATCH FOOTBALL/TV", "PLAY GOLF", "CELEBRATE/SMILE", "CALL HER", "GO TO CHURCH", "CRY", "8"],
+	points: [41, 17, 12, 8, 7, 7, 2, 0]
+}
+var question30 = {
+	question: "Tell me a traffic sign that best describes your love life",
+	answers: ["STOP/RED LIGHT", "YIELD", "GREEN LIGHT/GO", "SLOW", "ONE WAY", "CAUTION", "DEAD END", "8"],
+	points: [33, 24, 8, 8, 5, 5, 4, 0]
+}
+var question31 = {
+	question: "Name a language that makes anything sound sexy",
+	answers: ["FRENCH", "ITALIAN", "SPANISH", "BRITISH/ENGLISH", "PORTUGUESE", "GERMAN", "JAPANESE", "8"],
+	points: [40, 21, 11, 10, 9, 3, 3, 0]
+}
+var question32 = {
+	question: "Name a fruit that reminds you of a part of the human body",
+	answers: ["BANANA", "PEACH", "WATERMELON", "CANTALOUPE", "COCONUT", "EGGPLANT", "GRAPE", "8"],
+	points: [33, 24, 18, 9, 6, 4, 2, 0]
+}
+var question33 = {
+	question: "Name a profession that might sometimes require its members to be naked",
+	answers: ["STRIPPER", "MODEL", "ACTING", "PROSTITUTE", "5", "6", "7", "8"],
+	points: [61, 19, 9, 7, 0, 0, 0, 0]
+}
+var question34 = {
+	question: "Tell me something a man might do to get a woman 'in the mood'",
+	answers: ["KISS HER", "LIGHT CANDLES", "MASSAGE HER", "STRIP", "TURN DOWN LIGHTS", "BRING HER FLOWERS", "COOK FOR HER", "8"],
+	points: [28, 18, 17, 17, 8, 4, 4, 0]
+}
+var question35 = {
+	question: "Name something that you probably shouldn't send in an email",
+	answers: ["NAKED PICTURES", "PASSWORD", "SOCIAL SECURITY NUMBER", "VIRUS", "MONEY", "6", "7", "8"],
+	points: [34, 16, 13, 10, 5, 0, 0, 0]
+}
+var question36 = {
+	question: "Name a reason why parents say that childhood is better than adulthood",
+	answers: ["NO BILLS", "NO WORK", "NO WORRIES", "MORE FUN/PLAYING", "NO RESPONSIBILITY", "6", "7", "8"],
+	points: [53, 19, 12, 9, 6, 0, 0, 0]
+}
+var question37 = {
+	question: "Name something kids use for fun, but adults use for exercise",
+	answers: ["BIKE", "BALL", "JUMPROPE", "TRAMPOLINE", "SWIMMING POOL", "6", "7", "8"],
+	points: [48, 24, 18, 6, 3, 0, 0, 0]
+}
+var question38 = {
+	question: "Name something adults buy that can be very small or very large",
+	answers: ["TV", "CAR/VEHICLE", "HOUSE", "OVEN", "STEREO/SPEAKERS", "REFRIGERATOR", "7", "8"],
+	points: [36, 29, 21, 5, 4, 3, 0, 0]
+}
+var question39 = {
+	question: "Tell me something that responsible adults do on a regular basis",
+	answers: ["WORK", "PAY BILLS", "COOK", "CLEAN/LAUNDRY", "DOCTOR/DENTIST", "OIL CHANGE", "7", "8"],
+	points: [34, 25, 13, 13, 7, 6, 0, 0]
+}
+var question40 = {
+	question: "Name a Halloween Costume Adults Wear To Look Sexy",
+	answers: ["NURSE", "MAID", "DEVIL", "CAT", "WITCH", "VAMPIRE", "7", "8"],
+	points: [34, 31, 17, 11, 9, 4, 0, 0]
+}
+var question41 = {
+	question: "Name something a man might have in his pants when he is going on a hot date",
+	answers: ["Weanie Beanie", "Money", "Gum/Mints", "Wallet", "A Pitched Tent", "A Sock", "7", "8"],
+	points: [46, 23, 16, 6, 5, 2, 0, 0]
+}
+var question42 = {
+	question: "What do you think Steve Harvey/s Wife would say is the best thing about him?",
+	//answers: ["Humor/Personality", "Bae is Ballin", "Smile/Those XL Lips", "That Lickable Head", "His Laugh", "His Fine Threads", "Sexy Stache", "Character/Heart"],
+	answers: ["Humor/Personality", "Bae is Ballin", "Smile/Those XL Lips", "That Lickable Head", "His Laugh", "His Fine Threads", "Sexy Stache", "Character/Heart"],
+	points: [42, 24, 19, 4, 3, 2, 2, 2]
+}
+var question43 = {
+	question: "Grandpa says, Grandma used to make love. Now she likes to make what?",
+	answers: ["Cookies/Food", "Quilts/Cookies", "War/A Fuss", "Tea/Coffee", "ZZZ's/Sleep", "Egg Farts", "7", "8"],
+	points: [65, 5, 5, 4, 4, 3, 0, 0]
+}
+var question44 = {
+	question: "A cavewoman might say about a caveman, he does not talk much but he has a great big what?",
+	answers: ["Club/Stick", "Dongosaurus", "Mouth", "Heart", "Head", "Cave", "7", "8"],
+	points: [73, 6, 6, 3, 2, 2, 0, 0]
+}
+var question45 = {
+	question: "Name something a lifeguard might get fired for doing on the job",
+	answers: ["Sleeping", "Drinking", "Flirt/Mouth-to-Mouth", "Pee in Pool/On Beach", "Smoking Cigs", "Let People Drown", "Being on Phone", "8"],
+	points: [52, 15, 10, 9, 4, 4, 3, 0]
+}
+var question46 = {
+	question: "Name something that even adults hate to share",
+	answers: ["Money", "Food", "Bed", "Bathroom", "Clothes", "Secrets", "7", "8"],
+	points: [39, 34, 15, 4, 4, 3, 0, 0]
+}
+var question47 = {
+	question: "Name a Public Place where even an Adult can get lost easily",
+	answers: ["Mall", "Amusement Park", "Park", "Fair", "Parking Garage", "Airport", "7", "8"],
+	points: [45, 22, 10, 6, 5, 4, 0, 0]
+}
+var question48 = {
+	question: "Name an Olympic sport you'd watch more often if it were played in the nude",
+	answers: ["Volleyball", "Gymnastics", "Swimming", "Soccey", "Wrestling", "Tennis", "Track/Field", "8"],
+	points: [21, 20, 20, 18, 7, 5, 3, 0]
+}
+var question49 = {
+	question: "Name something a person on a diet might order at a restaurant",
+	answers: ["Salad", "Fish", "Chicken", "Water","Fruit/Veggies", "Eggs", "7", "8"],
+	points: [35, 27, 12, 10, 7, 3, 0, 0]
+}
+var questionSet = [question0, question1, question2, question3, question4, question5, question6, 
+question7, question8, question9, question10, question11, question12, question13, question14, question15,
+question16, question17, question18, question19, question20, question21, question22, question23, question24,
+question25, question26, question27, question28, question29, question30, question31, question32,
+question33, question34, question35, question36, question37, question38, question39, question40, question41,
+question42, question43, question44, question45, question46, question47, question48, question49];
+var totalNumber = 0;
+var correct = new Audio();
+correct.src = "correct.mp3";
+var wrong = new Audio();
+wrong.src = "wrong.mp3";
+var buzzer = new Audio();
+buzzer.src = "familyFeudBuzzer.mp3";
+var walkDown = new Audio();
+walkDown.src = "walkDown.mp3";
+
+// var chosenQuestion = document.getElementById("textbox1");
+// var index = parseInt(chosenQuestion.value);
+function revealQuestion() {
+	var chosenQuestion = document.getElementById("textbox1");
+	var revealQuestion = document.getElementById("displayQuestion");
+	var index = parseInt(chosenQuestion.value);
+	var question = questionSet[index];
+	revealQuestion.innerHTML = question.question.toString();
+}
+function sayHello() {
+	console.log("hello");
+}
+function revealAnswer1() {
+	var chosenQuestion = document.getElementById("textbox1");
+	var index = parseInt(chosenQuestion.value);
+	var question = questionSet[index];
+	var answer = question.answers[0];
+	var revealAnswer = document.getElementById("one");
+	one.innerHTML = answer + ": " + question.points[0].toString();
+	total = total + question.points[0];
+	updateTotal(total, 0);
+	correct.play();
+}
+function updateTotal(total, questionNum) {
+	var chosenQuestion = document.getElementById("textbox1");
+	var index = parseInt(chosenQuestion.value);
+	var total = document.getElementById("total");
+	var totalContent = parseInt(total.value);
+	console.log(questionSet[index].points[questionNum]);
+	console.log(total);
+	totalGUI.innerHTML = totalNumber + questionSet[index].points[questionNum];
+	totalNumber = totalNumber + questionSet[index].points[questionNum];
+	console.log(total);
+}
+function revealAnswer2() {
+	var chosenQuestion = document.getElementById("textbox1");
+	var index = parseInt(chosenQuestion.value);
+	var question = questionSet[index];
+	var answer = question.answers[1];
+	var revealAnswer = document.getElementById("two");
+	two.innerHTML = answer + ": " + question.points[1].toString();
+	total = total + question.points[1];
+	updateTotal(total, 1);
+	correct.play();
+}
+function revealAnswer3() {
+	var chosenQuestion = document.getElementById("textbox1");
+	var index = parseInt(chosenQuestion.value);
+	var question = questionSet[index];
+	var answer = question.answers[2];
+	var revealAnswer = document.getElementById("three");
+	three.innerHTML = answer + ": " + question.points[2].toString();
+	total = total + question.points[2];
+	updateTotal(total, 2);
+	correct.play();
+}
+function revealAnswer4() {
+	var chosenQuestion = document.getElementById("textbox1");
+	var index = parseInt(chosenQuestion.value);
+	var question = questionSet[index];
+	var answer = question.answers[3];
+	var revealAnswer = document.getElementById("four");
+	four.innerHTML = answer + ": " + question.points[3].toString();
+	total = total + question.points[3];
+	updateTotal(total, 3);
+	correct.play();
+}
+function revealAnswer5() {
+	var chosenQuestion = document.getElementById("textbox1");
+	var index = parseInt(chosenQuestion.value);
+	var question = questionSet[index];
+	var answer = question.answers[4];
+	var revealAnswer = document.getElementById("five");
+	five.innerHTML = answer + ": " + question.points[4].toString();
+	total = total + question.points[4];
+	updateTotal(total, 4);
+	correct.play();
+}
+function revealAnswer6() {
+	var chosenQuestion = document.getElementById("textbox1");
+	var index = parseInt(chosenQuestion.value);
+	var question = questionSet[index];
+	var answer = question.answers[5];
+	var revealAnswer = document.getElementById("six");
+	six.innerHTML = answer + ": " + question.points[5].toString();
+	total = total + question.points[5];
+	updateTotal(total, 5);
+	correct.play();
+}
+function revealAnswer7() {
+	var chosenQuestion = document.getElementById("textbox1");
+	var index = parseInt(chosenQuestion.value);
+	var question = questionSet[index];
+	var answer = question.answers[6];
+	var revealAnswer = document.getElementById("seven");
+	seven.innerHTML = answer + ": " + question.points[6].toString();
+	total = total + question.points[6];
+	updateTotal(total, 6);
+	correct.play();
+}
+function revealAnswer8() {
+	var chosenQuestion = document.getElementById("textbox1");
+	var index = parseInt(chosenQuestion.value);
+	var question = questionSet[index];
+	var answer = question.answers[7];
+	var revealAnswer = document.getElementById("eight");
+	eight.innerHTML = answer + ": " + question.points[7].toString();
+	total = total + question.points[7];
+	updateTotal(total, 7);
+	correct.play();
+}
+function playWrong() {
+	wrong.play();
+}
+function playBuzzer() {
+	buzzer.play();
+}
+function playShortTheme() {
+	walkDown.play();
+}
